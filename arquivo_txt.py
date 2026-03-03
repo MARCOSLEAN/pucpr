921408e97d3c7fa4eaeda66713916834ddc27356
Marcos Leandro Cachinski <47596246+MARCOSLEAN@users.noreply.github.com>
2026-03-03 20:41:44 -0300

    Atualização 01
    Foi alterado o código do arquivo_txt.py. Gerado um novo arquivo chamado dados.txt.

--- arquivo_txt.py
+++ arquivo_txt.py
@@ -1,11 +1,11 @@
 # Criar arquivo
-with open("infos.txt", "w") as arquivo:
+with open("dados.txt", "w") as arquivo:
     arquivo.write("Estudando DevOps!\n")
     arquivo.write("Entender Git é essencial.\n")
 
 # Ler arquivo
-with open("infos.txt", "r") as arquivo:
+with open("dados.txt", "r") as arquivo:
     conteudo = arquivo.read()
 
-print("Conteúdo do arquivo:")
+print("Conteúdo do arquivo dados.txt:")
 print(conteudo)
